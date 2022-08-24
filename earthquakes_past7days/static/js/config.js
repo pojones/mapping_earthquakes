122ebd3f6eaf993badc63c957fe1e15872f37842
Parker Jones <pojones@ucsc.edu>
2022-08-23 18:59:02 -0700

    uploading recent earthquake files and small adjustment to polygons file

---
+++ earthquakes_past7days/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoicG9qb25lcyIsImEiOiJjbDc1YzFscWYxcW1iM29tdmFkang5ZnZyIn0.Rbs3Vn8kNgfC9Q40prsO2g";
--- earthquakes_past7days/static/js/logic.js
+++ earthquakes_past7days/static/js/logic.js
@@ -106,9 +106,9 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -116,12 +116,8 @@ L.control.layers(baseMaps).addTo(map);
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/pojones/Mapping_Earthquakes/main/torontoNeighborhoods.json";
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
